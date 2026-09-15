a8fb2145c202fc18646a7818d0eddf3163f3b47c
FroiiLara07 <fgrimaldo@corpsierramadre.com>
2026-09-15 09:30:18 -0600

    feat: agregar endpoint ping para wake up desde app movil

--- Inscripciones UT/app.py
+++ Inscripciones UT/app.py
@@ -1147,5 +1147,10 @@ def api_recuperar_contrasena():
     return jsonify(ok=True, mensaje="Si el correo está registrado, recibirás un enlace."), 200
 
 
+
+@app.route('/ping')
+def ping():
+    return jsonify(ok=True, mensaje="pong"), 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
